08a6bfd4e996abcd1abab4d9cb5cd2a9d462987a
SokoAds <Mohamedsemm39@gmail.com>
2026-09-21 22:21:56 +0300

    Refresh HavanaAi PWA cache for new features

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="havanaai-v2";
+const CACHE="havanaai-v3";
 const ASSETS=["/","/index.html","/styles.css","/app.js","/manifest.json"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
