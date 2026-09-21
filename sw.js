46c19cbcd461ccb1ff891516e851c9fe21c3e2fc
SokoAds <Mohamedsemm39@gmail.com>
2026-09-21 22:35:23 +0300

    Refresh HavanaAi service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="havanaai-v4";
+const CACHE="havanaai-v5";
 const ASSETS=["/","/index.html","/styles.css","/app.js","/manifest.json"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
